a351f9c699227b2d99c0f3bd5d83acf31b86a070
Ajeett01 <abhitiwari501@gmail.com>
2024-04-21 23:08:21 +0530

    All recheck

--- tests/step-04/index.test.js
+++ tests/step-04/index.test.js
@@ -3,14 +3,12 @@ const { parseSelectQuery } = require('../../src/queryParser');
 const { executeSELECTQuery } = require('../../src/queryExecutor');
 
 test('Read CSV File', async () => {
-  const data = await readCSV('./sample.csv');
+  const data = await readCSV('./student.csv');
   expect(data.length).toBeGreaterThan(0);
-  expect(data.length).toBe(3);
+  expect(data.length).toBe(4);
   expect(data[0].name).toBe('John');
   expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
 });
-
-//test-04
 
 test('Parse SQL Query', () => {
   const query = 'SELECT id, name FROM student';
